eecbc3a276a6b8c08a6ca1bbb2d4aa65e0d26edf
bla3-afk <blazenuke3@gmail.com>
2026-03-14 16:19:18 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE_NAME = 'tripmaster-v1';
+const ASSETS = ['./', './index.html', './style.css', './script.js', 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css', 'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'];
+
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(res => res || fetch(e.request))));
